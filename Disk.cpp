3285ea5679b9a0e916eb103c6804be5f14b7d01b
Eduardo Catalán <71089107+MarvEdCV@users.noreply.github.com>
2021-08-24 20:15:29 -0600

    UPDATE MKDISK
    Se termino de perfeccionar el mkdisk para crear discos pensando en la mayoria de validaciones posibles para que funcione sin importar mayusculas o minusculas del comando ni tampoco del orden.

--- Disk.cpp
+++ Disk.cpp
@@ -7,6 +7,9 @@
 #include <time.h>
 #include <vector>
 #include <cctype>
+#include <iostream>
+#include <sstream>
+#include <cstdlib>
 using namespace std;
 using std::cout; using std::cin;
 using std::endl; using std::string;
@@ -17,7 +20,7 @@ struct Particion
 {
     char part_status;
     char part_type = 'p'; // primaria, extendida, logica
-    char part_fit = 'f'; //las opciones por default
+    string part_fit; //las opciones por default
     int part_start;
     int part_size;
     char part_name[16];
@@ -31,12 +34,13 @@ struct MBR
     Particion mbr_partition[4];
 };
 
-string path="url"; //url raiz para guardar
+string path=""; //url raiz para guardar
 
 struct DiscoD{ //estructura solo para tener la infomación de una entrada
 	string path="";
-	int size;
-	int unit; /// 1=kb 2=mb
+	float size;
+	string unit; /// K=kb M=mb
+    string fit; // BF= BEST FIT FF=FIRST FIT WF=WORST FIT
 }
 Disk1;
 void CrearDisco(DiscoD op){
@@ -53,7 +57,7 @@ cout<<"************************CREAR DISCO*************************"<<endl;
     //Tamaño para distintas unidades que se piden
     int tam;
 
-    if(op.unit==1){ //kb
+    if(op.unit=="K"){ //kb
       tam =  op.size* 1024;
     }
     else{ //mb
@@ -75,21 +79,46 @@ cout<<"************************CREAR DISCO*************************"<<endl;
     for(int i = 0; i < 4; i++){
         mbr.mbr_partition[i].part_status = '0';
         mbr.mbr_partition[i].part_size = 0;
-        mbr.mbr_partition[i].part_fit = 'f';
+        mbr.mbr_partition[i].part_fit = op.fit;
         mbr.mbr_partition[i].part_start = tam;
         strcpy(mbr.mbr_partition[i].part_name,"");
     }
-    cout<<"-------------DISCO CREADO--------------------"<<endl;
-    cout<<"Disco\nFecha de creacion: "<<asctime(gmtime(&mbr.mbr_fecha_creacion))<<endl;
+    cout<<"Disco nuevo :: "<<op.path<<"\nFecha de creacion: "<<asctime(gmtime(&mbr.mbr_fecha_creacion))<<endl;
     cout<<"Signature: "<<mbr.mbr_disk_signature <<endl;
-    cout<<"Tamaño: "<<mbr.mbr_tamano <<endl;
+    cout<<"Tamaño: "<<mbr.mbr_tamano <<"Bytes"<<endl;
     cout<<"Fit: " <<mbr.disk_fit <<endl;
+    cout<<"************************DISCO CREADO*************************"<<endl;
     ///escritura del mbr
     fseek(file,0,SEEK_SET);
     fwrite(&mbr,sizeof(MBR),1,file);
     fclose(file);
 }
 void Comands(){
+}
+vector<string> Splitequals(string text){
+    vector<string> lineSplit{};
+    string space = " ";
+    size_t pos =0;
+    while ((pos = text.find(space)) != string::npos) {
+        lineSplit.push_back(text.substr(0, pos));
+        text.erase(0, pos + space.length());
+    }
+    return lineSplit;
+}
+
+/**
+ * Funcion que retorna un vector spliteado por el simbolo = para poder sacar los parametros;
+ * */
+vector<string> SplitEqual(string p){
+    vector<string> PositionSplit{};
+    stringstream input_stringstream(p);
+    string l;
+    while (getline(input_stringstream,l, '='))
+    {
+        //cout << "Un valor: " << l << endl;
+        PositionSplit.push_back(l);
+    }
+    return PositionSplit;
 
 }
 vector<string> SplitSpace(string text){
@@ -142,11 +171,27 @@ int main(int argc, char const *argv[])
         string a = CastearMayuscula(CommandLine);//Casteamos todo a mayusculas para trabajarlo internamente porque pueden venir mayusculas y minusculas
         vector<string> lineSplit = SplitSpace(a);//Spliteamos por espacios
         if(lineSplit[0]=="MKDISK"){
-            
+            for(size_t i=1; i < lineSplit.size();i++){
+                vector<string> aux;
+                aux = SplitEqual(lineSplit[i]);
+                if(aux[0] == "-PATH"){
+                     Disk1.path = aux[1];
+                }
+                else if(aux[0]=="-F"){
+                    Disk1.fit= aux[1];
+                }
+                else if(aux[0]=="-U"){
+                     Disk1.unit = aux[1];
+                }
+                
+                else if(aux[0] == "-SIZE"){
+                    Disk1.size = stof(aux[1]);
+                }
+            }
+            CrearDisco(Disk1);
         }
-
-
     } while (Comparar(CommandLine,CLOSE)!=0);
+    
     return EXIT_SUCCESS;
 }
 
